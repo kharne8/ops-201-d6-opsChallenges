cd6e4e3bad0a374ae838247a781fe713229ffd69
kharne8 <cerasta81@gmail.com>
2023-02-08 00:59:12 +0000

    Ops Challenge Class 2 complete

--- helloWorld.sh
+++ helloWorld.sh
@@ -1,1 +1,14 @@
-#hello
+#!/bin/bash
+
+#Script: Ops 201 Class 02 Ops Challenge Solution
+#Author: Marco Aliaga
+#Date of latest revision: 7 FEB 2023
+#Purpose: Print a string to the terminal.
+
+#Main
+
+codeChallenge="I am working perfectly fine"
+
+echo $codeChallenge
+
+#End

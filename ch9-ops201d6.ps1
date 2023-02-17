0264a431260a14ce00b399d95b4ce264577cf6b4
kharne8 <cerasta81@gmail.com>
2023-02-17 04:08:33 +0000

    challenge complete

--- ch9-ops201d6.ps1
+++ ch9-ops201d6.ps1
@@ -6,10 +6,19 @@
 #Main
 # to tun on PowerShell => & "C:\Users\YOUR_USERNAME\Documents\NAME_OF_SCRIPT"
 
-Get-EventLog -LogName System -After (Get-Date).AddDays(-1) | Out-File "C:\Users\Marco\Desktop\last_24.txt"
+Output all events from System log from the last 24hrs into a txt file
+Get-EventLog -LogName System -After (Get-Date).AddDays(-1) | Format-List Souce, Message | Out-File "C:\Users\Marco\Desktop\last_24.txt"
 
-Get-EventLog -LogName System -EntryType Error (Get-Date).AddDays(-1) | Out-File "C:\Users\Marco\Desktop\errors.txt"
+Ouput all erros from System event log into a txt file
+Get-EventLog -LogName System -EntryType Error | Format-List Souce, Message | Out-File "C:\Users\Marco\Desktop\errors.txt"
 
+Print to the screen all events with a ID 16
+Get-EventLog -LogName System -InstanceId 16 | Format-List Souce, Message
 
+Print to the screen the most recent 20 entries on system event log
+Get-EventLog -LogName System -Newest 20 | Format-List Souce, Message
+
+Print to the screen the latest 500 entries without the ... and show entire text
+Get-EventLog -LogName System -Newest 500 | Format-List Souce, Message
 
 #End

e54659a421fa2a3eaca242e2431ded353ab6d11c
kharne8 <cerasta81@gmail.com>
2023-02-22 01:32:45 +0000

    chnllg completed

---
+++ ch12-ops201.ps1
@@ -0,0 +1,17 @@
+#Script: Ops 201 Class 12 Ops Challenge Solution
+#Author: Marco Aliaga
+#Date of latest revision: 21 FEB 2023
+#Purpose: Create a sting that uses Select-String that holds ipconfig/all and stores it on a txt file
+
+#Main
+
+#  > creates a network_report.txt and populated it with ipconfig /all results
+ipconfig /all > network_report.txt
+
+# I use select-string to look for a pattern that matches \d = number - 0 to 9, {1,3} = tells \d to match 1 to 3 digits, \. backlash escapes the dot and all together finds somehtign that ressembles ip address number
+Select-String -Path network_report.txt -Pattern "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" | foreach.Object {$_.Matches.value}
+
+# lastly we remove the txt file that I created
+Remove-Item network_report.txt
+
+#End

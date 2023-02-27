1bb8a04f54b8501adf3c94617cc62db6882c4c9b
kharne8 <cerasta81@gmail.com>
2023-02-26 23:57:05 -0500

    added function

--- ch12-ops201.ps1
+++ ch12-ops201.ps1
@@ -6,13 +6,21 @@
 #Main
 
 #  > creates a network_report.txt and populated it with ipconfig /all results
+# ADDED A FUNCTION
+createReport(){
 ipconfig /all > network_report.txt
+}
+
+# call func to create report
+createReport
 
 # I use select-string to look for a pattern that matches \d = number - 0 to 9, {1,3} = tells \d to match 1 to 3 digits, \. backlash escapes the dot and all together finds somehtign that ressembles ip address number
 Select-String -Path network_report.txt -Pattern "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" | foreach.Object {$_.Matches.value}
 
 # lastly we remove the txt file that I created
 Remove-Item network_report.txt
+
+
 
 # Streach Goals
 
